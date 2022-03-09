1c08f870219ed31a83dfb8544e8435b3d4ac7173
TecnoProfesor <65020790+TecnoProfesor@users.noreply.github.com>
2022-03-09 17:21:10 +0100

    Configurado para 5 microbits. Con sonido

--- main.ts
+++ main.ts
@@ -2,11 +2,14 @@ radio.onReceivedNumber(function (receivedNumber) {
     if (master == Math.trunc(receivedNumber / 10)) {
         if (receivedNumber % 10 == 1) {
             basic.showIcon(IconNames.Happy)
+            soundExpression.giggle.playUntilDone()
             Avanza(40)
             GiraIzq(40)
             Avanza(40)
             GiraIzq(40)
             basic.showIcon(IconNames.Heart)
+            basic.pause(5000)
+            basic.showString("")
         }
         if (receivedNumber % 10 == 2) {
             NeoPixel()
@@ -82,30 +85,6 @@ function GiraIzq (velocidad: number) {
     maqueen.motorStop(maqueen.Motors.M1)
     maqueen.motorStop(maqueen.Motors.M2)
 }
-input.onSound(DetectedSound.Loud, function () {
-    if (master == 1) {
-        radio.sendNumber(23)
-        basic.pause(1000)
-        basic.showString("F A N T E C 2 0 2 2")
-        radio.sendNumber(22)
-        NeoPixel()
-        for (let index = 0; index < 30; index++) {
-            pins.digitalWritePin(DigitalPin.P8, 1)
-            pins.digitalWritePin(DigitalPin.P12, 1)
-            basic.pause(100)
-            pins.digitalWritePin(DigitalPin.P8, 0)
-            pins.digitalWritePin(DigitalPin.P12, 0)
-            basic.pause(100)
-        }
-        radio.sendNumber(21)
-        basic.showIcon(IconNames.Happy)
-        Avanza(40)
-        GiraIzq(40)
-        Avanza(40)
-        GiraIzq(40)
-        basic.showIcon(IconNames.Heart)
-    }
-})
 function GiroDer (velocidad: number) {
     maqueen.motorRun(maqueen.Motors.M1, maqueen.Dir.CW, velocidad)
     maqueen.motorRun(maqueen.Motors.M2, maqueen.Dir.CCW, velocidad)
@@ -161,6 +140,45 @@ function Retrocede (velocidad: number) {
     maqueen.motorStop(maqueen.Motors.M1)
     maqueen.motorStop(maqueen.Motors.M2)
 }
+input.onLogoEvent(TouchButtonEvent.Touched, function () {
+    if (master == 1) {
+        radio.sendNumber(53)
+        basic.pause(1000)
+        radio.sendNumber(43)
+        basic.pause(1000)
+        radio.sendNumber(33)
+        basic.pause(1000)
+        radio.sendNumber(23)
+        basic.pause(1000)
+        basic.showString("F A N T E C 2 0 2 2")
+        radio.sendNumber(22)
+        radio.sendNumber(32)
+        radio.sendNumber(42)
+        radio.sendNumber(52)
+        NeoPixel()
+        for (let index = 0; index < 30; index++) {
+            pins.digitalWritePin(DigitalPin.P8, 1)
+            pins.digitalWritePin(DigitalPin.P12, 1)
+            basic.pause(100)
+            pins.digitalWritePin(DigitalPin.P8, 0)
+            pins.digitalWritePin(DigitalPin.P12, 0)
+            basic.pause(100)
+        }
+        radio.sendNumber(21)
+        radio.sendNumber(31)
+        radio.sendNumber(41)
+        radio.sendNumber(51)
+        basic.showIcon(IconNames.Happy)
+        soundExpression.giggle.playUntilDone()
+        Avanza(40)
+        GiraIzq(40)
+        Avanza(40)
+        GiraIzq(40)
+        basic.showIcon(IconNames.Heart)
+        basic.pause(5000)
+        basic.showString("")
+    }
+})
 function NeoPixel () {
     for (let index = 0; index < 5; index++) {
         RED = 0
@@ -200,6 +218,7 @@ let deroscuro = 0
 let izqoscuro = 0
 let master = 0
 let strip: neopixel.Strip = null
+music.setVolume(255)
 strip = neopixel.create(DigitalPin.P15, 4, NeoPixelMode.RGB)
 master = 2
 radio.setGroup(1)
